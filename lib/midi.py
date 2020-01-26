71bb0c4bb8b79394d159c8e5954dbd25c4efe797
Federico Dolce <psykopear@gmail.com>
2020-01-26 18:58:23 +0100

    Updated dependencies, added sample midi file

--- lib/midi.py
+++ lib/midi.py
@@ -1,15 +1,22 @@
 import mido
 
+
 class MidiOut:
     """
     Wrapper to make things easier with MIDI
     """
+
     def __init__(self):
-        self.port = mido.open_output()
+        self.output = mido.open_output()
         self.channel = 0
 
     def _send(self, msg_type, **kwargs):
-        self.port.send(mido.Message(msg_type, channel=self.channel, **kwargs))
+        print("Sending %s" % msg_type)
+        self.output.send(mido.Message(msg_type, channel=self.channel, **kwargs))
+
+    def send_msg(self, msg):
+        print("Sending message %s" % msg)
+        self.output.send(msg)
 
     def send_program_change(self, program):
         self._send("program_change", program=program)
@@ -22,5 +29,6 @@ class MidiOut:
 
     def send_note_off(self, note=60):
         self._send("note_off", note=note)
+
 
 midiout = MidiOut()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
+import mido
 import sys
+
 from time import sleep
 
 from PySide2.QtGui import QGuiApplication
@@ -7,39 +8,44 @@ from PySide2.QtQml import QQmlApplicationEngine
 from PySide2.QtCore import QObject, Slot, QThread
 
 from lib.midi import midiout
-        
+
 
 class MidiPlayer(QThread):
     """
     Thread used to send midi message while changing parameters
     so we can hear the effects
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.go = True
 
     def run(self):
-        while self.go:
-            midiout.send_note_on(40)
-            sleep(1)
-            midiout.send_note_off(40)
-            sleep(0.5)
+        while True:
+            for message in mido.MidiFile('song.mid').play():
+                if self.go:
+                    midiout.send_msg(message)
+        # while self.go:
+        #     midiout.send_note_on(40)
+        #     sleep(1)
+        #     midiout.send_note_off(40)
+        #     sleep(0.5)
 
 
 class MIDI(QObject):
     """
-    MIDI stuff helper: 
+    MIDI stuff helper:
       - send output
       - start and stop default MIDI playback
     """
-    
+
     @Slot()
     def stop_playback(self):
         self.thread.go = False
-    
+
     @Slot()
     def start_playback(self):
-        # I need to use self.thread to avoid the variable 
+        # I need to use self.thread to avoid the variable
         # being garbage collected before the end of the task
         self.thread = MidiPlayer()
         self.thread.finished.connect(self.thread.deleteLater)
@@ -49,12 +55,12 @@ class MIDI(QObject):
     def output(self, control, value):
         midiout.send_cc(control, value)
         print(f"Sending control:{control} value: {value}")
-        
 
-if __name__ == '__main__':
-    app = QGuiApplication([*sys.argv, '--style=qml/Docler'])
+
+if __name__ == "__main__":
+    app = QGuiApplication([*sys.argv, "--style=qml/Docler"])
     engine = QQmlApplicationEngine()
-    engine.load('qml/main.qml')
+    engine.load("qml/main.qml")
     if not engine.rootObjects():
         sys.exit(-1)
     context = engine.rootContext()
